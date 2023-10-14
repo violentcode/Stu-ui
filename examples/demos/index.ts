98accc59d1ac3bed3b0bd0b7e8b20f4c29612086
刘友汉 <cliu@liuyouhandeMacBook-Pro.local>
2023-10-14 11:18:09 +0800

    layout布局

--- examples/demos/index.ts
+++ examples/demos/index.ts
@@ -1,13 +1,13 @@
 import button from './button/index.vue'
 import _switch from './switch/index.vue'
-import popover from "./popover/index.vue"
-import sidebar from "./sidebar/index.vue"
-import layout from "./layout/index.vue"
+import popover from './popover/index.vue'
+import sidebar from './sidebar/index.vue'
+import layout from './layout/index.vue'
 
 export default [
   { title: 'Button 按钮', demo: button },
+  { title: 'Layout 布局', demo: layout },
   { title: 'Switch 开关', demo: _switch },
   { title: 'Popover 弹出框', demo: popover },
-  { title: 'Sidebar 侧边导航', demo: sidebar },
-  { title: 'Layout 布局', demo: layout },
+  { title: 'Sidebar 侧边导航', demo: sidebar }
 ]
--- examples/demos/layout/index.vue
+++ examples/demos/layout/index.vue
@@ -1,69 +1,90 @@
 <template>
-    <div class="layout-demo">
-        <div class="title">基础用法</div>
-        <div class="source">
-            <stu-row>
-                <stu-col span="24">span：24</stu-col>
-            </stu-row>
-            <stu-row>
-                <stu-col span="12">span：12</stu-col>
-                <stu-col span="12">span：12</stu-col>
-            </stu-row>
-            <stu-row>
-                <stu-col span="8">span：8</stu-col>
-                <stu-col span="8">span：8</stu-col>
-                <stu-col span="8">span：8</stu-col>
-            </stu-row>
-            <stu-row>
-                <stu-col span="6">span：6</stu-col>
-                <stu-col span="6">span：6</stu-col>
-                <stu-col span="6">span：6</stu-col>
-                <stu-col span="6">span：6</stu-col>
-            </stu-row>
-        </div>
-        <div class="title">列元素间距</div>
-        <div class="source">
-            <stu-row gutter="20">
-                <stu-col span="8">
-                    <stu-row  gutter="20">
-                        <stu-col span="8">span：8</stu-col>
-                        <stu-col span="8">span：8</stu-col>
-                        <stu-col span="8">span：8</stu-col>
-                    </stu-row>
-                </stu-col>
-                <stu-col span="8">span：8</stu-col>
-                <stu-col span="8">span：8</stu-col>
-            </stu-row>
-        </div>
+  <div class="layout-demo">
+    <div class="title">基础用法</div>
+    <div class="source">
+      <stu-row>
+        <stu-col span="24">span：24</stu-col>
+      </stu-row>
+      <stu-row>
+        <stu-col span="12">span：12</stu-col>
+        <stu-col span="12">span：12</stu-col>
+      </stu-row>
+      <stu-row>
+        <stu-col span="8">span：8</stu-col>
+        <stu-col span="8">span：8</stu-col>
+        <stu-col span="8">span：8</stu-col>
+      </stu-row>
+      <stu-row>
+        <stu-col span="6">span：6</stu-col>
+        <stu-col span="6">span：6</stu-col>
+        <stu-col span="6">span：6</stu-col>
+        <stu-col span="6">span：6</stu-col>
+      </stu-row>
     </div>
-</template>
-<script  lang="ts" setup>
-import { stuRow, stuCol } from "../../../dist/bundle.mjs"
+    <div class="title">列元素间距</div>
+    <div class="source">
+      <stu-row gutter="20">
+        <stu-col span="8"> span：8 </stu-col>
+        <stu-col span="8">span：8</stu-col>
+        <stu-col span="8">span：8</stu-col>
+      </stu-row>
+    </div>
+    <div class="title">对齐方式</div>
+    <div class="source">
+      <stu-row gutter="20" justify="center">
+        <stu-col span="6"> span：6 </stu-col>
+        <stu-col span="6">span：6</stu-col>
+        <stu-col span="6">span：6</stu-col>
+      </stu-row>
+      <!-- 右对齐 -->
+      <stu-row justify="end">
+        <stu-col span="6">span: 6</stu-col>
+        <stu-col span="6">span: 6</stu-col>
+        <stu-col span="6">span: 6</stu-col>
+      </stu-row>
 
+      <!-- 两端对齐 -->
+      <stu-row justify="space-between">
+        <stu-col span="6">span: 6</stu-col>
+        <stu-col span="6">span: 6</stu-col>
+        <stu-col span="6">span: 6</stu-col>
+      </stu-row>
+
+      <!-- 每个元素的两侧间隔相等 -->
+      <stu-row justify="space-around">
+        <stu-col span="6">span: 6</stu-col>
+        <stu-col span="6">span: 6</stu-col>
+        <stu-col span="6">span: 6</stu-col>
+      </stu-row>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { stuRow, stuCol } from '../../../dist/bundle.mjs'
 </script>
 <style scoped lang="less">
 .layout-demo {
-    color: #fff;
-    font-size: 14px;
+  color: #fff;
+  font-size: 14px;
 }
 
 :deep(.stu-row) {
-    margin-bottom: 10px;
+  margin-bottom: 10px;
 }
 
 :deep(.stu-col) {
-    text-align: center;
-    line-height: 36px;
-    height: 36px;
-    // border-radius: 4px;
-    background-clip: content-box
+  text-align: center;
+  line-height: 36px;
+  height: 36px;
+  // border-radius: 4px;
+  background-clip: content-box;
 }
 
 :deep(.stu-col):nth-child(2n) {
-    background-color: #215E21;
+  background-color: #215e21;
 }
 
 :deep(.stu-col):nth-child(2n + 1) {
-    background-color: #238E23;
+  background-color: #238e23;
 }
 </style>
