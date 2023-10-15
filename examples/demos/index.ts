318d9e0a3d6a01eb79681fdc7b674c9aee5da494
刘友汉 <cliu@liuyouhandeMacBook-Pro.local>
2023-10-15 22:05:37 +0800

    loading

--- examples/demos/index.ts
+++ examples/demos/index.ts
@@ -3,8 +3,11 @@ import _switch from './switch/index.vue'
 import popover from './popover/index.vue'
 import sidebar from './sidebar/index.vue'
 import layout from './layout/index.vue'
+import loading from './loading/index.vue'
 
 export default [
+  { title: 'Loading 加载', demo: loading },
+
   { title: 'Button 按钮', demo: button },
   { title: 'Layout 布局', demo: layout },
   { title: 'Switch 开关', demo: _switch },
---
+++ examples/demos/loading/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="stu-loading">
+    <svg class="circular" viewBox="25 25 50 50">
+      <circle cx="50" cy="50" r="20" fill="none" />
+    </svg>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="less">
+.circular {
+  display: block;
+  width: 100%;
+  height: 100%;
+  color: rgb(25, 137, 250);
+  circle {
+    animation: van-circular 1.5s ease-in-out infinite;
+    stroke: currentColor;
+    stroke-width: 3;
+    stroke-linecap: round;
+  }
+}
+
+@keyframes van-circular {
+  0% {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+  }
+
+  50% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -40;
+  }
+
+  100% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -120;
+  }
+}
+</style>
